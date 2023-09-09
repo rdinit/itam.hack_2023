ff0408f275f3b83b8846de69d6a497dfb213729f
Артем Иванов <m2311580@edu.misis.ru>
2023-09-09 12:53:02 +0300

    made some models that seem to work

--- project/models.py
+++ project/models.py
@@ -12,11 +12,23 @@ users_roles = db.Table('users_roles',
                        db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True)
                        )
 
+subtags = db.Table('subtags',
+                   db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+                   db.Column('subtag_id', db.Integer, db.ForeignKey('subtag.id'), primary_key=True)
+                   )
+
+tags = db.Table('tags',
+                db.Column('tagtype_id', db.Integer, db.ForeignKey('tagtype.id'), primary_key=True),
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+                )
+
 class Role(db.Model):
+    __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True)
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     roles = db.relationship('Role', secondary=users_roles, backref='users')
     email = db.Column(db.String(), unique=True)
@@ -29,7 +41,31 @@ class User(UserMixin, db.Model):
     #subtags = #many to many
 
 class Hour(db.Model):
+    __tablename__ = 'hour'
     id = db.Column(db.Integer, primary_key=True)
+
+
+class TagType(db.Model):
+    __tablename__ = 'tagtype'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    tags = db.relationship('Tag')
+    
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    tag_type_id = db.Column(db.Integer, db.ForeignKey('tagtype.id'))
+    approved = db.Column(db.Boolean(), default=True)
+    subtags = db.relationship('SubTag')
+    
+class SubTag(db.Model):
+    __tablename__ = 'subtag'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('type.id'))
+
+
 
 '''
 
