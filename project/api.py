0e77b97c3d2da208efbcc01ffbd14532196be075
Артем Иванов <m2311580@edu.misis.ru>
2023-09-10 17:00:44 +0300

    add api

--- project/api.py
+++ project/api.py
@@ -1,7 +1,85 @@
-from flask import Blueprint, request
-from .models import User
+from flask import Blueprint, request, jsonify
+from .models import User, Tag, SubTag
 from . import db
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 api = Blueprint('api', __name__)
+
+@api.route('/api/tags', methods=['PUT'])
+def add_tags():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    tag_id = request.json['id']
+    tag = Tag.query.filter_by(id=tag_id).first_or_404()
+    current_user.tags.append(tag)
+    db.session.commit()
+    return jsonify({'success': 'ok'})
+    
+@api.route('api/tags/<int:tag_id>', methods=['DELETE'])
+def delete_tag(tag_id):
+    tag = Tag.query.filter_by(id=tag_id).first_or_404()
+    if tag not in current_user.tags:
+        return jsonify({'error': 'user does not have tag'})
+    current_user.tags.remove(tag)
+    db.session.commit()
+    return jsonify({'success': 'ok'})
+
+@api.route('/api/subtags', methods=['PUT'])
+def add_subtags():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    subtag_id = request.json['id']
+    subtag = SubTag.query.filter_by(id=subtag_id).first_or_404()
+    tag = Tag.query.filter_by(id=subtag.tag_id).first_or_404()
+    if tag  not in current_user.tags:
+        return jsonify({'error': 'tag not added'})
+    current_user.subtags.append(subtag)
+    db.session.commit()
+    return jsonify({'success': 'ok'})
+    
+@api.route('api/subtags/<int:subtag_id>', methods=['DELETE'])
+def delete_subtag(subtag_id):
+    subtag = SubTag.query.filter_by(id=subtag_id).first_or_404()
+    if subtag not in current_user.tags:
+        return jsonify({'error': 'user does not have subtag'})
+    tag = Tag.query.filter_by(id=subtag.tag_id).first_or_404()
+    if tag  not in current_user.tags:
+        return jsonify({'error': 'tag not added'})
+    current_user.tags.remove(subtag)
+    db.session.commit()
+    return jsonify({'success': 'ok'})
+
+@api.route('api/user/friend', methods=['POST'])
+def add_friend():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    user_id = request.json['id']
+    user = User.query.filter_by(id=user_id).first_or_404()
+    current_user.friend(user)
+    db.session.commit()
+    return jsonify({'success':'ok'})
+
+@api.route('api/user/friend', methods=['DELETE'])
+def delete_friend():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    user_id = request.json['id']
+    user = User.query.filter_by(id=user_id).first_or_404()
+    current_user.unfriend(user)
+    db.session.commit()
+    return jsonify({'success':'ok'})
+
+@api.route('api/user/bio', methods=['PUT'])
+def update_bio():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    new_bio = request.json['bio']
+    current_user.bio = new_bio
+    db.session.commit()
+    return jsonify({'success':'ok'})
+
+
+    
+    
+    
