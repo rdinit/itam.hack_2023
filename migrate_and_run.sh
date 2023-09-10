3b1d6b62ea1828e7b107890772dab8cab02c583b
rdinit <28570294+rdinit@users.noreply.github.com>
2023-09-10 10:45:56 +0300

    migrate and run ony for running without docker

--- migrate_and_run.sh
+++ migrate_and_run.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=project
 export SECRET_KEY=test-string
-export SQLALCHEMY_DATABASE_URI='postgresql://postgres:password@postgres_server:5432/test1'
+export SQLALCHEMY_DATABASE_URI='postgresql://postgres:password@localhost:5432/test1'
 export FILL_DB=0
 flask db init
 flask db migrate
